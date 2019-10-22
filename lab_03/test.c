58b8a617e5d0213ca3e0f7830a5185e04f9dd0c4
paulpaulaga <tscheklin@gmail.com>
2019-10-22 12:44:17 +0300

    Test files functions updated

---
+++ lab_03/test.c
@@ -0,0 +1,33 @@
+//
+// Created by Павел Чеклин on 22.10.2019.
+//
+
+#include "test.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "time.h"
+
+#include "linked_list.h"
+#include "smatrix.h"
+#include "matrix.h"
+
+
+void linked_list_test()
+{
+    list_t l = NULL;
+    puts("CREATE");
+    for (size_t i = 0; i < 10; ++i)
+        l = list_add_tail(l, i);
+    puts("OUTPUT");
+    list_output(l);
+    puts("DELETE");
+    list_delete(l);
+}
+
+void smatrix_io_test()
+{
+    rare_matrix m;
+    s_matr_full_input(&m);
+    s_matr_output(m);
+}
---
+++ lab_03/test.h
@@ -0,0 +1,12 @@
+//
+// Created by Павел Чеклин on 22.10.2019.
+//
+
+#ifndef _TEST_H_
+#define _TEST_H_
+
+void linked_list_test();
+
+void smatrix_io_test();
+
+#endif //LAB_03_TEST_H
